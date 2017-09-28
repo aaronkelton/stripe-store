abdde25fa18e1650c311abe3e4ed41323fbe0d18
chemturion <aaron.k.kelton@gmail.com>
2017-09-28 13:35:52 -0500

    required jquery-rails and app.js manifest; used Stripes built in function setPubKey...

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,4 +1,7 @@
 $(function() {
+  stripe_public_key = $("meta[name='stripe-public-key']").attr("content");
+  Stripe.setPublishableKey(stripe_public_key);
+
   var $form = $('payment-form');
   $form.submit(function(event) {
     // Disable the submit button to prevent repeated clicks:
